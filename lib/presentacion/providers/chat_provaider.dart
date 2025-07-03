a808db79e0bc6cac0efa1951b32f5dc5fa36afaa
kerberus <arturoeddin@gmail.com>
2025-07-03 13:46:17 -0500

    3 de julio  en la Mañana

--- lib/presentacion/providers/chat_provaider.dart
+++ lib/presentacion/providers/chat_provaider.dart
@@ -1,21 +1,33 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:yes_no_app2/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
   List<Message> messageList = [
     Message(text: "Hola Amor", fromWho: FromWho.me),
     Message(text: "Ya regresate del trabajo?", fromWho: FromWho.me)
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
 
-      if (text.isEmpty) return;
+    final newMessage = Message(
+      text: text,
+      fromWho: FromWho.me,
+    );
+    messageList.add(newMessage);
+    notifyListeners();
+    moveScrollToBottom();
+  }
 
-       final newMessage = Message(
-        text: text,
-        fromWho: FromWho.me,
-      );
-      messageList.add(newMessage);
-      notifyListeners();     
+  Future<void> moveScrollToBottom() async{
+    await Future.delayed(const Duration(milliseconds: 100));
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
   }
 }
